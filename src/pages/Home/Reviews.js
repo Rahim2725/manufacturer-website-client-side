140b8d2627b825cac75754fd018dc5d803090245
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-26 18:14:09 +0600

    review add

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Sherad/Loading';
+import Review from './Review';
 
 const Reviews = () => {
+  
+
+    // const { data: tools, isLoading, refetch } = useQuery('tools', () => fetch('https://damp-island-71958.herokuapp.com/service').then(res => res.json()))
+
+    const { data: reviews, isLoading, refetch } = useQuery('users', () => fetch('tools.json').then(res => res.json()))
+
+    
+    if(isLoading){
+        return <Loading></Loading>
+    }
+    
     return (
-        <div>
-            <h2>Reviews ALL </h2>
+        <div className='text-center mt-32'>
+            <h1 className='text-3xl '>Tools</h1>
+
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 mt-5'>
+                {
+                    reviews.map(review => <Review
+                    key={review._id}
+                    review={review}
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };

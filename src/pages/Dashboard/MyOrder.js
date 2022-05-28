e58c434f4c0d27add9482142774809c41ba2798c
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-29 02:57:59 +0600

    delete done

--- src/pages/Dashboard/MyOrder.js
+++ src/pages/Dashboard/MyOrder.js
@@ -1,44 +1,62 @@
 import { signOut } from 'firebase/auth';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import Loading from '../Sherad/Loading';
+import OrderRow from './OrderRow';
 
 const MyOrder = () => {
-    const [orders, setOrders] = useState([]);
-
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
-    
 
-    useEffect(() => {
-        if (user) {
-            fetch(`http://localhost:5000/purchase?email=${user.email}`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-                .then(res => {
-                    console.log(res)
-                    if (res.status === 401 || res.status === 403) {
-                        signOut(auth);
-                        localStorage.removeItem('accessToken')
-                        navigate('/')
-                    }
-
-                    return res.json()
-                })
-                .then(data => {
-
-                    setOrders(data)
-                })
+    const { data: orders, isLoading, refetch } = useQuery(['users', user], () => fetch(`http://localhost:5000/purchase?email=${user.email}`, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }, [user, navigate])
+    }).then(res => {
+        if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem('accessToken')
+            navigate('/')
+        }
+
+        return res.json()
+    }))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
 
     return (
         <div>
             <h1>My order: {orders.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Position</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => <OrderRow
+                                key={order._id}
+                                order={order}
+                                refetch={refetch}
+                            ></OrderRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Dashboard/OrderRow.js
@@ -0,0 +1,30 @@
+import React  from 'react';
+
+const OrderRow = ({order, refetch}) => {
+const {name, price, userName, position, _id } = order ;
+
+
+    const handleDelete = id => {
+        fetch(`http://localhost:5000/purchase?id=${id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            refetch()
+        })
+    }
+
+    return (
+        <tr>
+            <td>{userName}</td>
+            <td>{name}</td>
+            <td>{price}</td>
+            <td>{position === 'deliver' ? 'Deliver' : 'Pending...' }</td>
+            <td>{position !== 'deliver' && <button onClick={() => handleDelete(_id)} className='btn btn-sm btn-secondary '>Delete</button> }</td>
+
+        </tr>
+    );
+};
+
+export default OrderRow;

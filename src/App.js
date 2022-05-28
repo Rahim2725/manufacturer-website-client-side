0cdee745de9e015f1f1796de015cf33495517c4b
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-28 22:15:45 +0600

    require auth added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
+import RequireAuth from './pages/Login/RequireAuth';
 import SignUp from './pages/Login/SignUp';
 import Purchase from './pages/Purchase/Purchase';
 import Footer from './pages/Sherad/Footer';
@@ -18,7 +19,11 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
-        <Route path='/purchase/:purchaseId' element={<Purchase></Purchase>}></Route>
+        <Route path='/purchase/:purchaseId' element={
+        <RequireAuth>
+          <Purchase></Purchase>
+        </RequireAuth>
+      }></Route>
       </Routes>
       <Footer></Footer>
       <ToastContainer></ToastContainer>

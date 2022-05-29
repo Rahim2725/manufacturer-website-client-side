4b9d4f5961081f020a69e601180da0693c1af160
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-29 17:30:30 +0600

    add review

--- src/pages/Dashboard/AddReview.js
+++ src/pages/Dashboard/AddReview.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
-const AddReview = ({review}) => {
+const AddReview = ({ review }) => {
 
     const [user] = useAuthState(auth)
-    
+
     const addProduct = event => {
-        event.preventDefault() ;
-        const description = event.target.description.value ;
-        const rating = event.target.rating.value ;
+        event.preventDefault();
+        const description = event.description.value;
+        const rating = event.target.rating.value;
 
         const product = {
-           description: description,
-           rating: rating,
+            description: description,
+            rating: rating,
         }
 
-        fetch('http://localhost:5000/product', {
+        fetch('http://localhost:5000/review', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
-                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
             },
             body: JSON.stringify(product),
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            event.target.reset() ;
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                event.target.reset();
+            })
 
     }
 
@@ -38,17 +38,10 @@ const AddReview = ({review}) => {
                 <div class="card-body">
                     <h2 class=" text-2xl text-center">Add Product</h2>
                     <form onSubmit={addProduct}  >
-                        <input type="text" placeholder="Name" name='name' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
 
-                        <input type="number" placeholder="Product Price" name='price' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
+                        <textarea placeholder='Review' className=' input-primary border-2 border-primary' name="description" id="" cols="30" rows="10"></textarea>
 
-                        <textarea type="text" placeholder="Product Details" name='description' class="input mb-3 block h-16 input-bordered input-primary w-full max-w-xs" required />
-
-                        <input type="text" placeholder="Available Quantity" name='available_quantity' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
-
-                        <input type="text" placeholder="Minimum Order Quantity" name='minimum_order_quantity' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
-
-                        <input type="text" placeholder="Image URL" name='img' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
+                        <input type="number" placeholder="Rating" name='rating' class="input mb-3 block input-bordered input-primary w-full max-w-xs" required />
 
                         <input className='btn btn-primary w-full' type="submit" value="Add" />
                     </form>
